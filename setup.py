d7dc33509fb519cd1c42a4ebdd60970dab517dbf
Aanchal Choudhary <aanchalchoudhary31@hotmail.com>
2026-04-24 15:03:23 +0530

    init: project structure and setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pylock",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "bcrypt",
+        "pyperclip"
+    ],
+    entry_points={
+        "console_scripts": [
+            "pylock=pylock.main:main"
+        ]
+    }
+)
